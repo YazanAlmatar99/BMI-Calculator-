21ef228a29fe7c8554817e8f1c3e1b58dbe3b642
Yazan <yazanmatar90@gmail.com>
2020-04-24 21:01:13 -0400

    Moved to MVC design

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
+    var calculatorBrain = CalculatorBrain()
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
     
-    var bmiValue:String?
     
     override func viewDidLoad() {
            super.viewDidLoad()
@@ -29,11 +29,9 @@ class CalculateViewController: UIViewController {
         weightLabel.text = "\(weight)Kg"
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
-        let height =  heightSlider.value
-        let weight =  weightSlider.value
-        let bmi = weight / (height * height)
-        bmiValue = String(format:"%.1f",bmi)
-        print(bmi)
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        calculatorBrain.calculateBMI(height:height,weight:weight)
         //this will perform the transition
         self.performSegue(withIdentifier: "goToResults", sender: self)
     }
@@ -41,7 +39,9 @@ class CalculateViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResults" {
             let destinationVC = segue.destination as! ResultViewController
-            destinationVC.bmiValue = bmiValue
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
 }
--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -3,7 +3,31 @@
 //  BMI Calculator
 //
 //  Created by Yazan Almatar on 4/24/20.
-//  Copyright © 2020 Angela Yu. All rights reserved.
+//  Copyright © 2020 Yazan Almatar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+struct CalculatorBrain {
+    var bmi:BMI?
+    mutating func calculateBMI(height:Float,weight:Float){
+        let bmiValue  = weight / (height * height)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+        } else if bmiValue > 18.5 && bmiValue < 24.9 {
+           bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
+        } else if bmiValue > 24.0 {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
+        }
+    }
+    func getBMIValue() -> String {
+        let bmiValueFormatted = String(format:"%.1f",bmi?.value ?? 0.0)
+        return bmiValueFormatted
+    }
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+}
+
